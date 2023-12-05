632ffc03b2b1ad829aa2ebf504e8000442c15585
Ville Penttinen <villem.penttinen@gmail.com>
2023-12-05 07:59:09 +0200

    Implement support for additional pooled objects

---
+++ src/Visp.Common/PooledDictionary.cs
@@ -0,0 +1,55 @@
+// Copyright 2023 Ville Penttinen
+// Distributed under the MIT License.
+// https://github.com/vipentti/visp-fs/blob/main/LICENSE.md
+
+using Microsoft.Extensions.ObjectPool;
+
+namespace Visp.Common;
+
+public static class PooledDictionary
+{
+    public static void ReturnToPool<K, V>(this Dictionary<K, V> it) where K : notnull
+        => PooledDictionary<K, V>.Return(it);
+
+    public static Dictionary<K, V> Get<K, V>() where K : notnull
+        => PooledDictionary<K, V>.Get();
+
+    public static PooledDisposer<Dictionary<K, V>> GetPooled<K, V>() where K : notnull
+        => new(
+            PooledDictionary<K, V>.Get(),
+            ReturnToPool
+        );
+}
+
+public static class PooledDictionary<K, V>
+    where K: notnull
+{
+    internal class PooledDictionaryPolicy : PooledObjectPolicy<Dictionary<K, V>>
+    {
+        public int InitialCapacity { get; set; } = 100;
+
+        public int MaximumAllowedItemCount { get; set; } = 2048;
+
+        public override Dictionary<K, V> Create() => new(InitialCapacity);
+
+        public override bool Return(Dictionary<K, V> obj)
+        {
+            if (obj.Count > MaximumAllowedItemCount)
+            {
+                return false;
+            }
+
+            obj.Clear();
+            return true;
+        }
+    }
+
+    private static readonly ObjectPool<Dictionary<K, V>> s_pool = new DefaultObjectPool<Dictionary<K, V>>(new PooledDictionaryPolicy());
+
+    public static Dictionary<K, V> Get() => s_pool.Get();
+
+    public static void Return(Dictionary<K, V> obj)
+    {
+        s_pool.Return(obj);
+    }
+}
---
+++ src/Visp.Common/PooledList.cs
@@ -0,0 +1,72 @@
+// Copyright 2023 Ville Penttinen
+// Distributed under the MIT License.
+// https://github.com/vipentti/visp-fs/blob/main/LICENSE.md
+
+using System.Collections.Immutable;
+using Microsoft.Extensions.ObjectPool;
+
+namespace Visp.Common;
+
+public static class PooledList
+{
+    public static void ReturnToPool<T>(this List<T> obj) =>
+        PooledList<T>.Return(obj);
+
+    public static T[] ToArrayAndReturn<T>(this List<T> s) =>
+        PooledList<T>.ToArrayAndReturn(s);
+
+    public static ImmutableArray<T> ToImmutableAndReturn<T>(this List<T> s) =>
+        PooledList<T>.ToImmutableAndReturn(s);
+
+    public static List<T> Get<T>() => PooledList<T>.Get();
+
+    public static PooledDisposer<List<T>> GetPooled<T>() =>
+        new(Get<T>(), ReturnToPool);
+}
+
+public static class PooledList<T>
+{
+    internal class PooledListPolicy : PooledObjectPolicy<List<T>>
+    {
+        public int InitialCapacity { get; set; } = 100;
+
+        public int MaximumRetainedCapacity { get; set; } = 4096;
+
+        public override List<T> Create() => new(InitialCapacity);
+
+        public override bool Return(List<T> obj)
+        {
+            if (obj.Capacity > MaximumRetainedCapacity)
+            {
+                return false;
+            }
+
+            obj.Clear();
+            return true;
+        }
+    }
+
+    private static readonly ObjectPool<List<T>> s_pool = new DefaultObjectPool<List<T>>(new PooledListPolicy());
+
+    public static List<T> Get() => s_pool.Get();
+
+    public static void Return(List<T> obj)
+    {
+        s_pool.Return(obj);
+    }
+
+    public static T[] ToArrayAndReturn(List<T> s)
+    {
+        var ret = s.ToArray();
+        Return(s);
+        return ret;
+    }
+
+    public static ImmutableArray<T> ToImmutableAndReturn(List<T> s)
+    {
+        var ret = ImmutableArray.CreateBuilder<T>(s.Capacity);
+        ret.AddRange(s);
+        Return(s);
+        return ret.MoveToImmutable();
+    }
+}
