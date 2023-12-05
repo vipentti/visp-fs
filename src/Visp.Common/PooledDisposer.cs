049eadf92bd54c5916fa98c9f1d7c89f7fb51270
Ville Penttinen <villem.penttinen@gmail.com>
2023-12-05 07:59:09 +0200

    Implement support for additional pooled objects

---
+++ src/Visp.Common/PooledDisposer.cs
@@ -0,0 +1,29 @@
+// Copyright 2023 Ville Penttinen
+// Distributed under the MIT License.
+// https://github.com/vipentti/visp-fs/blob/main/LICENSE.md
+
+namespace Visp.Common;
+
+/// <summary>
+/// Automatically call the given function when disposed with the
+/// original value. Intended for use with pooled objects so they
+/// can be automatically returned to the pool after use.
+/// </summary>
+/// <typeparam name="T">Type of object contained</typeparam>
+/// <param name="value">Value</param>
+/// <param name="ret">Function to call when disposed</param>
+public readonly struct PooledDisposer<T>(
+    T value,
+    Action<T> ret
+) : IDisposable
+{
+    /// <summary>
+    /// The value
+    /// </summary>
+    public readonly T Value { get; } = value;
+
+    public void Dispose()
+    {
+        ret(Value);
+    }
+}
