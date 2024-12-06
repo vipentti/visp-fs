2b606e2e85a2db5c4caf8d61c42c9f8df715e748
Ville Penttinen <villem.penttinen@gmail.com>
2024-12-06 07:45:46 +0200

    feat(aoc2024): day6 part1

--- aoc2024.ps1
+++ aoc2024.ps1
@@ -6,11 +6,11 @@ param (
     [switch] $All,
     [switch] $NoBuild,
     [ValidateSet(
-        "day1", "day2", "day3", "day4", "day5"
-        #, "day6", "day7", "day8", "day9"
-        # , "day10", "day11", "day12", "day13", "day14", "day15", "day16"
-        # , "day17", "day18", "day19", "day20", "day21", "day22"
-        # , "day23", "day24", "day25"
+        "day1", "day2", "day3", "day4", "day5", "day6"
+        , "day7", "day8", "day9"
+        , "day10", "day11", "day12", "day13", "day14", "day15", "day16"
+        , "day17", "day18", "day19", "day20", "day21", "day22"
+        , "day23", "day24", "day25"
     )]
     [string] $Day
 )
